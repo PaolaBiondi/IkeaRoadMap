6c151bac060cc43f834ec05dd6273cc491154d62
Paola <ml3oepl0@duck.com>
2026-09-16 00:06:43 +0200

    Init of an project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type Department = {
+  id: number;
+  name: string;
+  description: string;
+};
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +15,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('MyIkeaRoadMap');
+    selectedDepartment: Department | null = null;
+     departments: Department[] = [
+    {
+      id: 12,
+      name: 'Posuđe',
+      description: 'Odjel s posuđem za pečenje, posluživanje i kuhanje.'
+    },
+    {
+      id: 20,
+      name: 'Ugradbena rasvjeta',
+      description: 'Odjel s rasvjetom za kuhinje i druge prostore.'
+    }
+  ];
+
+  openDepartment(id: number): void {
+    this.selectedDepartment =
+      this.departments.find(department => department.id === id) ?? null;
+  }
+
+  closeDetails(): void {
+    this.selectedDepartment = null;
+  }
 }
